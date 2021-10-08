b236741f38e492376873fe6db198c23e4cb978db
montbowie <montbowie1999@hotmail.com>
2021-10-08 18:24:29 +0700

    first commit

---
+++ application/views/mont_view.php
@@ -0,0 +1,303 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Mont Profile</title>
+    <meta name="description" content="" />
+    <meta name="author" content="Tooplate" />
+    
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <link rel="stylesheet" href="assets/css/tooplate-style.css" />
+    <link rel="stylesheet" href="assets/css/owl.css" />
+    <link rel="stylesheet" href="assets/css/lightbox.css" />
+  </head>
+
+
+  <body>
+    <div id="page-wraper">
+      
+      <div class="responsive-nav">
+        <i class="fa fa-bars" id="menu-toggle"></i>
+        <div id="menu" class="menu">
+          <i class="fa fa-times" id="menu-close"></i>
+          <div class="container">
+            <div class="image">
+              <a href="#"><img src="assets/images/mont.jpg" alt="" /></a>
+            </div>
+            <div class="author-content">
+              <h4>Mont Nonubon</h4>
+              <span>SANG</span>
+            </div>
+            <nav class="main-nav" role="navigation">
+              <ul class="main-menu">
+                <li><a href="#section1">About Me</a></li>
+                <li><a href="#section2">Study Progress</a></li>
+                <li><a href="#section3">Portforlio</a></li>
+                <li><a href="#section4">Contact</a></li>
+              </ul>
+            </nav>
+            <div class="social-network">
+              <ul class="soial-icons">
+                <li>
+                  <a href="https://www.facebook.com/bowieInfinity9/"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/n.mont9/"><i class="fa fa-instagram"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/channel/UC3_jI0JhSfkcyeOHV57tUXg"><i class="fa fa-youtube"></i></a>
+                </li>
+                
+              </ul>
+            </div>
+            <div class="copyright-text">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="section about-me" data-section="section1">
+        <div class="container">
+        <div class="top-header">
+        	<img src="assets/images/mont1.jpg" alt="" />
+        </div>
+          <div class="section-heading">
+          
+            <h2>About Me</h2>
+            <div class="line-dec"></div>
+            <span>My name is Mont nonubon. My nickname is sang.
+                    I am 21 years old. I come from tak. I stady at Rajamangala University of Technology Lanna Tak. I am third years student.</span>
+          </div>
+          <div class="right-image-post">
+            <div class="row">
+ 
+              <div class="col-md-8">
+                <div class="left-text">
+                  <h4>My Objective</h4>
+                  <p>Looking for a challenging role in areputable organization to utilize mytechnical, database, andmanagement skills</p>
+                  <p>For the growth ofthe organization as well as toenhance my knowledge about new and emerging trends in the IT sector.</p>
+                  
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="right-image">
+                  <img src="assets/images/n.jpg" alt="" />
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          
+        </div>
+      </section>
+
+      <section class="section my-services" data-section="section2">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Study Progress</h2>
+            <div class="line-dec"></div>
+            <span>My knowledge in University. When I study in 3 years</span>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-database"></i>
+                <h4>Database</h4>
+                <p>Learn about how to create a database in laravel and some basic database information.</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-mobile"></i>
+                <h4>Android Application</h4>
+                <p>Study about how to create application android in android studio.</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-code"></i>
+                <h4>HTML</h4>
+                <p>Learn basic HTML to create website until use to build css.</p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-item">
+                <i class="fa fa-file-code-o"></i>
+                <h4>Java</h4>
+                <p>Learn alot about Java from year 1 to years 3 . Use Java in many work it the one I get it or most.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section my-work" data-section="section3">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Portfolio</h2>
+            <div class="line-dec"></div>
+            <span>My work in Rajamangala University Of Technology in the last 3 years.</span>
+          </div>
+          <div class="row">
+            <div class="isotope-wrapper">
+              <form class="isotope-toolbar">
+                <label><input type="radio" data-type="*" checked="" name="isotope-filter" />
+                  <span>all</span></label>
+                <label><input type="radio" data-type="Video Editor" name="isotope-filter" />
+                  <span>Video Editor</span></label>
+                <label><input type="radio" data-type="Photoshop" name="isotope-filter" />
+                  <span>Photoshop</span></label>
+                <label><input type="radio" data-type="Camera Man" name="isotope-filter" />
+                  <span>Camera Man</span></label
+                >
+              </form>
+              <div class="isotope-box">
+                <div class="isotope-item" data-type="Video Editor">
+                  <figure class="snip1321">
+                    <img src="assets/images/1.jpg" alt="" />
+                    <figcaption>
+                      <a href="https://www.youtube.com/watch?v=IzFP8A9Q928&t=22s">
+                      <h4>View Project</h4>
+                    </figcaption>
+                  </figure>
+                </div>
+
+                <div class="isotope-item" data-type="Video Editor">
+                  <figure class="snip1321">
+                    <img src="assets/images/2.PNG" alt="" />
+                    <figcaption>
+                      <a href="https://www.youtube.com/watch?v=6o_9URU1WdI&t=136s">                       
+                      <h4>View Project</h4>
+                      
+                    </figcaption>
+                  </figure>
+                </div>
+
+                <div class="isotope-item" data-type="Video Editor">
+                  <figure class="snip1321">
+                    <img src="assets/images/3.jpg" alt="" />
+                    <figcaption>
+                      <a href="https://www.youtube.com/watch?v=Zi-tQIX1lDY">                 
+                      <h4>View Project</h4>   
+                    </figcaption>
+                  </figure>
+                </div>
+
+                <div class="isotope-item" data-type="Camera Man">
+                  <figure class="snip1321">
+                    <img src="assets/images/4.jpg" alt="" />
+                    <figcaption>                  
+                      <a href="assets/images/4.jpg">                             
+                      <h4>View Project</h4>                     
+                    </figcaption>
+                  </figure>
+                </div>
+
+                <div class="isotope-item" data-type="Video Editor , Camera Man">
+                  <figure class="snip1321">
+                    <img src="assets/images/6.PNG" alt="" />
+                    <figcaption>
+                      <a href="https://www.youtube.com/watch?v=cmbWi3ehlF4&t=19s">                       
+                      <h4>View Project</h4>
+                      
+                    </figcaption>
+                  </figure>
+                </div>
+
+                <div class="isotope-item" data-type="Photoshop">
+                  <figure class="snip1321">
+                    <img src="assets/images/7.PNG" alt="" />
+                    <figcaption>
+                      <a href="assets/images/7.PNG">    
+                      <h4>View Project</h4>
+                    </figcaption>
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section contact-me" data-section="section4">
+        <div class="container">
+          <div class="section-heading">
+            <h2>Contact</h2>
+            <div class="line-dec"></div>
+            <span>Email :  Montbowie1999@hotmail.com </span>
+              <br>
+            <span>Phone :  0943044112 </span>
+              <br>
+            <span>Address : 29/34 Tak, Thailand 63000 </span>
+          </div>
+          
+        </div>
+      </section>
+    </div>
+    
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+      
+      $(".main-menu li:first").addClass("active");
+
+      var showSection = function showSection(section, isAnimate) {
+        var direction = section.replace(/#/, ""),
+          reqSection = $(".section").filter(
+            '[data-section="' + direction + '"]'
+          ),
+          reqSectionPos = reqSection.offset().top - 0;
+
+        if (isAnimate) {
+          $("body, html").animate(
+            {
+              scrollTop: reqSectionPos
+            },
+            800
+          );
+        } else {
+          $("body, html").scrollTop(reqSectionPos);
+        }
+      };
+
+      var checkSection = function checkSection() {
+        $(".section").each(function() {
+          var $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+          if (topEdge < wScroll && bottomEdge > wScroll) {
+            var currentId = $this.data("section"),
+              reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+            reqLink
+              .closest("li")
+              .addClass("active")
+              .siblings()
+              .removeClass("active");
+          }
+        });
+      };
+
+      $(".main-menu").on("click", "a", function(e) {
+        e.preventDefault();
+        showSection($(this).attr("href"), true);
+      });
+
+      $(window).scroll(function() {
+        checkSection();
+      });
+    </script>
+  </body>
+</html>
